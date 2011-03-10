d6fb6434651193bedc5f8f05f54f70b0fcf43a81
cerebis <matt.demaere@gmail.com>
2011-03-10 11:16:33 +1100

    Database initialization for roles

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into Roles (role_id,authority,name) values(nextval('hibernate_sequence'),'ROLE_USER','user');
+insert into Roles (role_id,authority,name) values(nextval('hibernate_sequence'),'ROLE_ADMIN','admin');
