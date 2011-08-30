657af2e3beab46bc200558d2b23d7d93e843dcc1
cerebis <matt.demaere@gmail.com>
2011-08-30 10:15:02 +1000

    Removed Biojava from dependency stack

--- bin/prep_repo.sh
+++ bin/prep_repo.sh
@@ -12,14 +12,6 @@ mvn install:install-file \
 	-DgeneratePom=true
 
 mvn install:install-file \
-	-Dfile=local-lib/biojava3-core-3.0.2-SNAPSHOT.jar \
-	-DgroupId=shap-extra \
-	-DartifactId=biojava3-core \
-	-Dversion=3.0.2-SNAPSHOT \
-	-Dpackaging=jar \
-	-DgeneratePom=true
-	
-mvn install:install-file \
 	-Dfile=local-lib/commons-cli-2.0-beta2-SNAPSHOT.jar \
 	-DgroupId=shap-extra \
 	-DartifactId=commons-cli \
